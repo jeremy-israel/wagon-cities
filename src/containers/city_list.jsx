0156a065d834e492eff3ec77cd91e2854f43ec14
jeremy israel <israel.jeremy@gmail.com>
2017-09-26 17:36:25 +0200

    working reducer for citieslist

--- src/containers/city_list.jsx
+++ src/containers/city_list.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import City from './city';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { setCities } from '../actions';
 
 class CityList extends Component {
 
   componentWillMount() {
-    console.log('YEAH MOFO COMPNENT WILL MOUNT');
+    // console.log('YEAH MOFO COMPNENT WILL MOUNT');
+    this.props.setCities();
   }
   render(){
     return (
@@ -19,4 +23,15 @@ class CityList extends Component {
   }
 }
 
-export default CityList;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { setCities: setCities },
+  dispatch );
+}
+
+function mapStateToProps(state) { return {
+    cities: state.cities
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CityList);
